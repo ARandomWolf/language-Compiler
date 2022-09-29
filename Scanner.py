6af207dc92d6d62893e9578cbba7ed58b9afcf27
wyatt woof <wwolf959@gmail.com>
2022-09-29 15:44:25 -0500

    fixed EOF bug

--- Scanner.py
+++ Scanner.py
@@ -90,7 +90,9 @@ def scan_for_tokens(lines):
             elif current_state != 0:
                 new_token += lines[ln][cn]
 
-    token_list.append(Token(basic_tokenID[4], 'EOF', len(lines), 1))
+    if token_list[len(token_list)-1].tk_string != 'EOF':
+        token_list.append(Token(basic_tokenID[4], 'EOF', len(lines), 1))
+
     return add_unique_token_ids(token_list)
 
 
