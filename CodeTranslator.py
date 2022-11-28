f4c1fefee1c1aaa7c0274a4b129d4f1737fd6f59
wyatt woof <wwolf959@gmail.com>
2022-11-28 17:06:16 -0600

    refactored class names

--- CodeTranslator.py
+++ CodeTranslator.py
@@ -11,12 +11,12 @@ class Local_scope:
         self.tree_root = treeroot
         self.varCount = 0
 
-    def check_scope(self):
-        self.preorder(self.tree_root)
+    def check_local_scope(self):
+        self.traverse_parse_tree(self.tree_root)
         print('\nVariable declarations and scope validated!')
         return
 
-    def preorder(self, root):
+    def traverse_parse_tree(self, root):
         count = 0
         if root is None:
             return
@@ -26,24 +26,27 @@ class Local_scope:
 
             # if child is a vars node, add declared vars to stack
             if child is not None and child.name == 'vars':
-                count = self.get_vars(child)
+                count = self.add_vars_to_stack(child)
 
             else:
                 if child is not None:
                     for tk in child.data:
                         if tk.tokenID == 'IDENT_tk':
-                            #print('Valid ID Token FOUND: ' + tk.tk_string)
+                            # print('Valid ID Token FOUND: ' + tk.tk_string)
                             # make sure variable is declared before use
                             if not self.stack.__contains__(tk.tk_string):
-                                print ('Error!\nVariable not declared! line ' + str(tk.line_num) +' character ' + str(tk.character_num))
+                                print ('Error!\nVariable not declared! line ' + str(tk.line_num) +
+                                       ' character ' + str(tk.character_num))
                                 exit(1)
-                self.preorder(child)
+                self.traverse_parse_tree(child)
 
         i = 0
         for i in range(count):
             self.stack.pop()
 
-    def get_vars(self,vars_node):
+    # adds all variable names in a vars node or any of its children to stack.
+    # return type  is number of variables added to stack.
+    def add_vars_to_stack(self, vars_node):
         var_count =0
         if vars_node.name == 'vars':
             if self.stack.__contains__(vars_node.data[0].tk_string):
